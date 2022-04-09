93360eea3d03cffa33c1c5676d1d83a2f9c6b1af
sajal gupta <sajal00gupta@gmail.com>
2022-04-09 17:30:26 +0530

    s

--- cms/netlify/hooks/beforeBuild.js
+++
@@ -1,9 +0,0 @@
-import generatePostApi from '../build/generate-post-api'
-import generateCategoryApi from '../build/generate-category-api'
-import copyStaticToDist from '../build/copy-static-to-dist'
-
-export default () => {
-  generatePostApi()
-  generateCategoryApi()
-  copyStaticToDist()
-}
